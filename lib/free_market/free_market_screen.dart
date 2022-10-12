5460675b4e165efdd94bfdb4531fa5865996983c
kenji-axia <114896398+kenji-axia@users.noreply.github.com>
2022-10-12 13:31:48 +0900

    App Barの実装

--- lib/free_market/free_market_screen.dart
+++ lib/free_market/free_market_screen.dart
@@ -7,7 +7,16 @@ class FreeMarketScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color.fromRGBO(254, 254, 254, 1),
+        title: const Text('出品'),
+        titleTextStyle: const TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.black87,
+          fontSize: 18,
+        ),
+      ),
       body: SafeArea(
         child: DefaultTextStyle(
           style: const TextStyle(color: Colors.white),
