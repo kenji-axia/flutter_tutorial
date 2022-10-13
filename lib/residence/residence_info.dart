68bcb56ea8f2537a6aec8c76ce88e172efc9b4f8
kenji-axia <114896398+kenji-axia@users.noreply.github.com>
2022-10-13 15:26:14 +0900

    データクラスの作成

---
+++ lib/residence/residence_info.dart
@@ -0,0 +1,19 @@
+class ResidenceInfo {
+  final String name;
+  final String price;
+  final String place;
+  final String size;
+  final String buildingInfo;
+  final String residenceInsideImagePath;
+  final String residenceOutsideImagePath;
+
+  ResidenceInfo({
+    required this.name,
+    required this.price,
+    required this.place,
+    required this.size,
+    required this.buildingInfo,
+    required this.residenceInsideImagePath,
+    required this.residenceOutsideImagePath,
+  });
+}
--- lib/residence/residence_screen.dart
+++ lib/residence/residence_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_tutorial/constants.dart';
 
+import 'residence_info.dart';
 import 'components/residence_app_bar.dart';
 import 'components/category_filtering_section.dart';
 import 'components/category_content.dart';
@@ -9,7 +10,28 @@ import 'components/residence_floating_action_button.dart';
 import 'components/residence_bottom_navigation_bar.dart';
 
 class ResidenceScreen extends StatelessWidget {
-  const ResidenceScreen({Key? key}) : super(key: key);
+  ResidenceScreen({Key? key}) : super(key: key);
+
+  final List<ResidenceInfo> _dummyResidenceInfoData = [
+    ResidenceInfo(
+      name: 'Rising place川崎',
+      price: '2,000万円',
+      place: '京急本線 京急川崎駅 より 徒歩9分',
+      size: '1K / 21.24㎡ 南西向き',
+      buildingInfo: '2階/15階建 築5年',
+      residenceInsideImagePath: 'images/residence_inside.jpg',
+      residenceOutsideImagePath: 'images/residence_outside.jpg',
+    ),
+    ResidenceInfo(
+      name: 'Rising place川崎',
+      price: '2,000万円',
+      place: '京急本線 京急川崎駅 より 徒歩9分',
+      size: '1K / 21.24㎡ 南西向き',
+      buildingInfo: '2階/15階建 築5年',
+      residenceInsideImagePath: 'images/residence_inside.jpg',
+      residenceOutsideImagePath: 'images/residence_outside.jpg',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,26 +45,21 @@ class ResidenceScreen extends StatelessWidget {
             color: Colors.black87,
           ),
           child: ListView(
-            children: const [
+            children: [
               CategoryFilteringSection(),
-              CategoryContent(
-                name: 'Rising place川崎',
-                price: '2,000万円',
-                place: '京急本線 京急川崎駅 より 徒歩9分',
-                size: '1K / 21.24㎡ 南西向き',
-                buildingInfo: '2階/15階建 築5年',
-                residenceInsideImagePath: 'images/residence_inside.jpg',
-                residenceOutsideImagePath: 'images/residence_outside.jpg',
-              ),
-              CategoryContent(
-                name: 'Rising place川崎 2',
-                price: '2,000万円',
-                place: '京急本線 京急川崎駅 より 徒歩9分',
-                size: '1K / 21.24㎡ 南西向き',
-                buildingInfo: '2階/15階建 築5年',
-                residenceInsideImagePath: 'images/residence_inside.jpg',
-                residenceOutsideImagePath: 'images/residence_outside.jpg',
-              ),
+              for (ResidenceInfo residenceInfo in _dummyResidenceInfoData) ...{
+                CategoryContent(
+                  name: residenceInfo.name,
+                  price: residenceInfo.price,
+                  place: residenceInfo.place,
+                  size: residenceInfo.size,
+                  buildingInfo: residenceInfo.buildingInfo,
+                  residenceInsideImagePath:
+                      residenceInfo.residenceInsideImagePath,
+                  residenceOutsideImagePath:
+                      residenceInfo.residenceOutsideImagePath,
+                )
+              }
             ],
           ),
         ),
